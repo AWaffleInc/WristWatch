c1d943baed12306d4e428e8317f5e86973837911
Edward Wang <37031713+ewang2002@users.noreply.github.com>
2021-04-10 21:35:57 -0700

    Fixed a typo

--- ImageProcessor/main.py
+++ ImageProcessor/main.py
@@ -1,21 +1,20 @@
 import os
 import cv2
 
+# Define the directory here.
 TARGET_DIRECTORY = os.path.join("", "", "")
 
 
 def main() -> None:
-    for file in os.listdir(TARGET_DIRECTORY):
+    for file_name in os.listdir(TARGET_DIRECTORY):
         # If it starts with "processed_" then we don't need to process this image.
         # If this image does not end with .png or .jpg, then it's not an image file.
-        if file.startswith("processed_") or (not file.endswith(".png") and not file.endswith(".jpg")):
+        if file_name.startswith("processed_") or (not file_name.endswith(".png") and not file_name.endswith(".jpg")):
             continue
 
-        img_file_path = os.path.join(TARGET_DIRECTORY, "processed_" + file)
-
-        img = cv2.imread(img_file_path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(os.path.join(TARGET_DIRECTORY, file_name), cv2.IMREAD_GRAYSCALE)
         resized_image = cv2.resize(img, (100, 100))
-        cv2.imwrite(img_file_path, resized_image)
+        cv2.imwrite(os.path.join(TARGET_DIRECTORY, "processed_" + file_name), resized_image)
 
 
 if __name__ == '__main__':
